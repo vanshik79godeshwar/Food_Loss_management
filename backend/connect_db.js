f2d1847fb7bb1ed1eb023d58ad435c709b679251
Khush Dhameliya <khushdhameliya007@gmail.com>
2025-01-09 17:00:04 +0530

    connect database

---
+++ backend/connect_db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connect_db = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Connected to the database');
+
+    }
+    catch(error){
+        console.log('Error connecting to the database', error);
+    }
+}
+
+module.exports = connect_db; 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require('express')
 require('dotenv').config()
 const app=express();
 const port=3000;
+const connect_db = require('./connect_db');
 
 // Middlewares.
 app.use(express.urlencoded({extended:true}))
@@ -18,7 +19,10 @@ app.use((err,req,res,next)=>{
     return res.status(500).json({message:'Internal Server Error!'})
 })
 
+connect_db();
+
 app.listen(port,()=>{
+    
     console.log('App is listening on port successfully :)');
 })
 
